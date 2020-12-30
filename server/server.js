8b42039ba488c3522cc3bf645d82c06c05d05ea4
Vitaly K <kuvini@gmail.com>
2020-12-30 16:58:14 +0500

    Husky test

--- server/server.js
+++ server/server.js
@@ -37,6 +37,8 @@ if (config.socketsEnabled) {
   })
 }
 
+const x
+
 if (config.mongoEnabled) {
   console.log('MongoDB Enabled: ', config.mongoEnabled)
   mongoose.connect(config.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
