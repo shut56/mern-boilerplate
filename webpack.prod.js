3e19e39eaae98e7b1dbdc5dbd99b797e938a051b
Vitaly K <kuvini@gmail.com>
2021-12-19 23:54:09 +0500

    Add TerserPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,13 +4,16 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 const config = {
   mode: 'production',
   optimization: {
     minimize: true,
     minimizer: [
-      () => ({
+      new TerserPlugin({
+        parallel: true,
+        minify: TerserPlugin.esbuildMinify,
         terserOptions: {
           compress: {
             drop_console: true
